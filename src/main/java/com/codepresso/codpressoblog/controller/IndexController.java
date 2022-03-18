479aafd17bc68620ff277c0400ccd9b40fbc6912
suubinkim <0vbbbv0@gmail.com>
2022-03-18 16:18:28 +0900

    글 목록 더보기 기능 ( 페이지 ) 2

--- src/main/java/com/codepresso/codpressoblog/controller/IndexController.java
+++ src/main/java/com/codepresso/codpressoblog/controller/IndexController.java
@@ -16,9 +16,10 @@ public class IndexController {
     private final PostService postService;
 
     //포스트 리스트를 모델에 추가하여 인덱스로 전달
+    //처음에는 첫 페이지이기 때문에 세개의 포스트만 전달
     @RequestMapping(value = "/")
     public String index(Model model) {
-        List<Post> allPost = postService.getAllPost();
+        List<Post> allPost = postService.getPostByPage(1,3);
         model.addAttribute("posts", allPost);
         return "index";
     }
